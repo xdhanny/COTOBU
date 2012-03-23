b5960c98e57e445ba6843cfa7d6f0eba14def1de
DaniG <xdhanny@gmail.com>
2012-03-23 20:13:45 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+COTOBU::Application.config.secret_token = '6bc1adf27f74ea21370fefc64974f453b1c66207544f1f4c84f8ddaa4c5a35ba0b97c725bb77483c90a324fe5a9ba52b5c715c9d2eb7f3f1f049e051f8360bc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+COTOBU::Application.config.session_store :cookie_store, key: '_COTOBU_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# COTOBU::Application.config.session_store :active_record_store
